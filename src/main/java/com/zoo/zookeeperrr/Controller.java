8052e9f2032669be91891120799120fc05a2bec8
Bedra Kraja <bedra.kraja22@gmail.com>
2022-10-06 00:32:49 +0200

    Finished

--- src/main/java/com/zoo/zookeeperrr/Controller.java
+++ src/main/java/com/zoo/zookeeperrr/Controller.java
@@ -8,36 +8,83 @@ import javax.persistence.Id;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-
     @Autowired
-    private AnimalsRepository animalsRepository;
+    private AnimalsRepository AnimalsRepository;
 
-    @GetMapping("/animal")
-    public List<Animals> getAllAnimal() {
-        return animalsRepository.findAll();
+    @GetMapping("/Animals")
+    public List<Animals> getAllAnimals() {
+        return AnimalsRepository.findAll();
     }
 
-    @Entity
-    class Animals {
-
-        @Id
-        @GeneratedValue
-        public Integer id;
-        public String name;
-        public String species;
-        public String origin;
-        public Boolean isHungry;
-
-        public Animals() {
-        }
+    @PostMapping("/Animals")
+    public Animals createNewAnimals(@RequestBody Animals AnimalsData) {
+        return AnimalsRepository.save(AnimalsData);
     }
 
-    interface AnimalsRepository extends JpaRepository<Animals, Integer> {
+    @DeleteMapping("/Animals/{id}")
+    public String deleteAnimals(@PathVariable Integer id) {
+        AnimalsRepository.deleteById(id);
+        return "Shush";
     }
 
+    @PatchMapping("/Animals/{id}")
+    public Animals updateAnimals(@RequestBody Animals AnimalsData, @PathVariable Integer id) {
+        AnimalsData.id = id;
+        return AnimalsRepository.save(AnimalsData);
+    }
 }
+
+@Entity
+class Animals {
+    @Id
+    @GeneratedValue
+    public Integer id;
+    public String name;
+    public String species;
+    public String origin;;
+    public Boolean isHungry;
+
+    public Animals() {
+    }
+}
+
+interface AnimalsRepository extends JpaRepository<Animals, Integer> {
+}
+// @GetMapping("/animal")
+// public List<Animals> getAllAnimal() {
+// return animalsRepo.findAll();
+// }
+
+// @GetMapping("/animal")
+// public List<Animals> getAllAnimals() {
+// return animalsRepo.findAll();
+// }
+
+// @Entity
+// class Animals {
+
+// @Id
+// @GeneratedValue
+// public Integer id;
+// public String name;
+// public String species;
+// public String origin;;
+// public Boolean isHungry;
+
+// public Animals() {
+// }
+// }
+// }
+
+// interface AnimalsRepo extends JpaRepository<Animals, Integer> {
+// }
